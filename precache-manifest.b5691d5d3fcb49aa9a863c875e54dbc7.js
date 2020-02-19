ada8214ef09301e2c5beeb7fbbb90ced64978e87
CN24 <codenation@CN24s-MacBook-Pro.local>
2020-02-19 14:25:24 +0000

    Updates

---
+++ precache-manifest.b5691d5d3fcb49aa9a863c875e54dbc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3afe342ffcc5c751e9b7456f3c6b989",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "921907b2e4e96e469c62",
+    "url": "/calculator/static/css/main.f3bd2d3c.chunk.css"
+  },
+  {
+    "revision": "d07630b46d49ae788210",
+    "url": "/calculator/static/js/2.8f07b515.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.8f07b515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921907b2e4e96e469c62",
+    "url": "/calculator/static/js/main.411b2b2e.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
